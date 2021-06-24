c795c38be1f12b368bf8b6946ff4b796c4ebca5e
Bassam Naeem <57295738+Bassamnaeem@users.noreply.github.com>
2021-06-25 01:18:27 +0500

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -17,7 +17,7 @@ const greeting = {
     "A passionate Full Stack Software Developer 👨‍💻 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing",
+    "https://drive.google.com/file/d/1Rp8XqCBdGNZ91ZXd4NKuaZ7ajcIe9S4_/view?usp=sharing",
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
